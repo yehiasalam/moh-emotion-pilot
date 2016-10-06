5effa241b3d97c69d336184e358af9dcde5482bd
Yehia A.Salam <yehiasalam@live.com>
2016-10-06 18:44:32 +0200

    - getting the data and license files from User Documents instead of local ClickOnce dir

--- FilePath.cs
+++ FilePath.cs
@@ -12,13 +12,13 @@ namespace MoHEmotionPilot
     {
         static public String GetClassifierDataFolder()
         {
-            return Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "lib", "affectiva-classifier-data");
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "moh-emotion-pilot", "affectiva-classifier-data");
 
         }
 
         static public String GetAffdexLicense()
         {
-            var path = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "affdex.license"); ;
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "moh-emotion-pilot", "affdex.license"); ;
 
             return File.ReadAllText(path);
         }
